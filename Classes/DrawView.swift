2039a75be3d8ad3523d46351fbcc9dd169312853
Frank Michael Sanchez <orion1701@me.com>
2014-12-24 10:43:15 -0500

    Improved DrawView with example usage and animation

--- Classes/DrawView.swift
+++ Classes/DrawView.swift
@@ -28,6 +28,7 @@ class DrawView: UIView {
     var strokeColor: UIColor = UIColor.blackColor()
     var strokeWidth: CGFloat = 25.0
     var debugingBox: Bool = false
+    var animationDuration: Double = 3.0
     private var paths: [UIBezierPath] = []
     private var bezierPath: UIBezierPath = UIBezierPath()
     private var signLine: UIBezierPath?
@@ -37,31 +38,31 @@ class DrawView: UIView {
     // MARK: Init
     override init() {
         super.init()
-        self.setupDrawView()
+        self.setupCanvas()
     }
     override init(frame: CGRect) {
         super.init(frame: frame)
-        self.setupDrawView()
+        self.setupCanvas()
     }
-    
     required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        self.setupDrawView()
+        self.setupCanvas()
     }
     // MARK: Class setup
-    private func setupDrawView() {
+    private func setupCanvas() {
         self.backgroundColor = UIColor.whiteColor()
         self.signLine = UIBezierPath()
         self.signLine?.lineCapStyle = kCGLineCapRound
         self.signLine?.lineWidth = 3.0
         // Draw the "x" for the line
-        self.signLine?.moveToPoint(CGPointMake(20.0, self.frame.size.height-30))
-        self.signLine?.moveToPoint(CGPointMake(30.0, self.frame.size.height-40))
-        self.signLine?.moveToPoint(CGPointMake(30.0, self.frame.size.height-30))
-        self.signLine?.moveToPoint(CGPointMake(20.0, self.frame.size.height-40))
+        self.signLine?.moveToPoint(CGPointMake(20.0, self.frame.size.height-30.0))
+        self.signLine?.addLineToPoint(CGPointMake(30.0, self.frame.size.height-40.0))
+        self.signLine?.moveToPoint(CGPointMake(30.0, self.frame.size.height-30.0))
+        self.signLine?.addLineToPoint(CGPointMake(20.0, self.frame.size.height-40.0))
         // Draw the actual line for the signature mode
-        self.signLine?.moveToPoint(CGPointMake(20, self.frame.size.height-20))
-        self.signLine?.addLineToPoint(CGPointMake(self.frame.size.width-20, self.frame.size.height-20))
+        self.signLine?.moveToPoint(CGPointMake(20.0, self.frame.size.height-20.0))
+        self.signLine?.addLineToPoint(CGPointMake(self.frame.size.width-20.0, self.frame.size.height-20.0))
+        self.setNeedsDisplay()
     }
     override func drawRect(rect: CGRect) {
         if !isAnimating{
@@ -112,7 +113,78 @@ class DrawView: UIView {
     func drawBezierPath(path: UIBezierPath) {
         self.drawPath(path.CGPath)
     }
-    // MARK: Touch tracking
+    func clearCanvas() {
+        self.bezierPath = UIBezierPath()
+        self.paths.removeAll(keepCapacity: false)
+        self.setNeedsDisplay()
+        self.setupCanvas()
+    }
+    func refreshCanvas() {
+        self.setupCanvas()
+    }
+    // MARK Undo support
+    func undoLastPath() {
+        self.paths.removeLast()
+        self.setNeedsDisplay()
+    }
+    // MARK View Draw Reading
+    func imageRepresentation() -> UIImage {
+        UIGraphicsBeginImageContext(self.bounds.size)
+        let context = UIGraphicsGetCurrentContext()
+        self.layer.renderInContext(context)
+        let viewImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return viewImage
+    }
+    func bezierPathRepresentation() -> UIBezierPath {
+        var singleBezierPath = UIBezierPath()
+        if self.paths.count > 0 {
+            for path in paths {
+                singleBezierPath.appendPath(path)
+            }
+        }else{
+            singleBezierPath = self.bezierPath
+        }
+        return singleBezierPath
+    }
+    // MARK: Animation
+    func animateCanvas() {
+        var animatingPath = UIBezierPath()
+        if self.editingMode == .Editable {
+            for path in self.paths {
+                animatingPath.appendPath(path)
+            }
+        }else{
+            animatingPath = self.bezierPath
+        }
+        // Clear out the existing view
+        self.isAnimating = true
+        self.setNeedsDisplay()
+        // Create shapre layer that stores the path
+        self.animationLayer = CAShapeLayer()
+        self.animationLayer?.fillColor = nil
+        self.animationLayer?.path = animatingPath.CGPath
+        self.animationLayer?.frame = self.frame
+        self.animationLayer?.strokeColor = self.strokeColor.CGColor
+        self.animationLayer?.lineWidth = self.strokeWidth
+        self.animationLayer?.miterLimit = 0.0
+        self.animationLayer?.lineCap = "round"
+        // Create animation of path
+        let animation = CABasicAnimation()
+        animation.duration = self.animationDuration
+        animation.fromValue = 0.0
+        animation.toValue = 1.0
+        animation.delegate = self
+        self.animationLayer?.addAnimation(animation, forKey: "strokeEnd")
+        self.layer.addSublayer(self.animationLayer?)
+    }
+    override func animationDidStop(anim: CAAnimation!, finished flag: Bool) {
+        self.isAnimating = false
+        self.animationLayer?.removeFromSuperlayer()
+        self.animationLayer = CAShapeLayer()
+        self.setNeedsDisplay()
+    }
+    // MARK: Touch Tracking
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
         if self.editingMode == .Editable {
             self.bezierPath = UIBezierPath()
--- Example-Swift/Example-Swift/ViewController.swift
+++ Example-Swift/Example-Swift/ViewController.swift
@@ -10,15 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mainDrawView: DrawView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        var drawView = DrawView()
+        self.mainDrawView.drawingMode = .Signature
+        // Need to do a refresh since the view has adapted to the current device size.
+        self.mainDrawView.refreshCanvas()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillTransitionToSize(size: CGSize, withTransitionCoordinator coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransitionToSize(size, withTransitionCoordinator: coordinator)
+        self.mainDrawView.refreshCanvas()
+    }
+    @IBAction func animate(sender: AnyObject) {
+        self.mainDrawView.animateCanvas()
+    }
+    
+    
+    // For iOS 7 support
+    override func didRotateFromInterfaceOrientation(fromInterfaceOrientation: UIInterfaceOrientation) {
+        self.mainDrawView.refreshCanvas()
+    }
+    override func viewDidLayoutSubviews() {
+        self.mainDrawView.refreshCanvas()
     }
 
 
